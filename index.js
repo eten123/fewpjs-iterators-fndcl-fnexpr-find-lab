01d257d650c28e135b090ee5702ebc40b1da414e
Eten <eten.uket@yale.edu>
2021-08-02 11:42:43 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+
+
+function superbowlWin(objectArray){ //objectArray is a placeholder for a random array we may choose to input
+    const winYear = objectArray.find(obj => {
+      return obj.result === "W" //grabs the first object that has a w in the year for the result tab
+    })
+  
+    return (!!winYear) ? winYear.year : undefined  //ternary expression, if the the win years equal eachother then return win year else return undefined
+  }
