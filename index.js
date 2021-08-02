99d2c1fb37b6d7a6ffb47a2eefe694c087c611ad
Eten <eten.uket@yale.edu>
2021-08-02 11:40:42 -0400

    commit tree dcf49e40bee3bce0484a9af9442b81a926d5427d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"},
+    {year: "2015", result: "L"},
+]
 
+
+function superbowlWin(objectArray){
+    const winYear = objectArray.find(obj => {
+      return obj.result === "W"
+    })
+  
+    return (!!winYear) ? winYear.year : undefined
+  }
